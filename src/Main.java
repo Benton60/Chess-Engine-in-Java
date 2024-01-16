b96ed6eb3a9acd99137096d354147805e53fc495
Benton60 <145625002+Benton60@users.noreply.github.com>
2024-01-15 21:22:28 -0500

    The GUI Now has live updates. Everything is ready to begin work on the engine.

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,61 @@
-import javax.imageio.ImageIO;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageFilter;
-import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
 
-import static java.awt.image.BufferedImage.*;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static int chess_Board_X_Value = 95;
-    public static int chess_Board_Y_Value = 188;
-    public static int chess_Board_Height = 704;
-    public static int chess_Board_Width = 704;
-    public static String[][] chessboard = new String[8][8];
-    public static HashMap<Integer, String> pieceCorrelations = new HashMap<Integer, String>();
-    public static double time = 0;
-    public static void main(String[] args) {
-        time = System.nanoTime();
-        pieceCorrelations.put(28, "BR");//Black Rook
-        pieceCorrelations.put(54, "Bk");//Black knight
-        pieceCorrelations.put(31, "BB");//Black Bishop
-        pieceCorrelations.put(44, "BQ");//Black Queen
-        pieceCorrelations.put(43, "BK");//Black King
-        pieceCorrelations.put(27, "BP");//Black Pawn
-        pieceCorrelations.put(21, "WR");//White Rook
-        pieceCorrelations.put(42, "Wk");//White knight
-        pieceCorrelations.put(24, "WB");//White Bishop
-        pieceCorrelations.put(10, "WQ");//White Queen
-        pieceCorrelations.put(50, "WK");//White King
-        pieceCorrelations.put(19, "WP");//White Pawn
-        pieceCorrelations.put(0, "  "); //This is for empty squares, so they don't say 'null' when the array is printed. Cuz that's annoying
-
-        checkForPieces();
-        System.out.println("\n\n\n");
-
-        for (String[] strings : chessboard) {
-            for (String string : strings) {
-                System.out.print(string + " ");
+    public static JFrame frame = new JFrame();
+    public static JPanel panel = new JPanel() {
+        @Override
+        public void paint(Graphics g) {
+            g.setColor(Color.white);
+            g.fillRect(0, 0, 500, 500);
+            g.setColor(Color.black);
+            g.drawString("Time: " + String.valueOf((System.nanoTime()-time)/1000000000),10,20);
+            for(int i = 1; i <= 8; i++){
+                for(int j = 1; j <= 8; j++){
+                    g.drawString(chessboard[j-1][i-1],i*20, j*20+50);
+                }
             }
-            System.out.println();
         }
-        //checkForPieces(1,1);
-        System.out.println((System.nanoTime()-time)/1000000000);
+    };
+    public static int chess_Board_X_Value = 95; //This is the top right X point of the chess board and is used to calculate
+                                                //piece positions and where to move the mouse.
+    public static int chess_Board_Y_Value = 188; //This is the top right Y point of the chess board and is used to calculate
+                                                 //piece positions and where to move the mouse.
+    public static int chess_Board_Height = 704; //Total height of the chess board in pixels
+    public static int chess_Board_Width = 704; //Total width of the chess board in pixels
+    public static String[][] chessboard = new String[8][8]; //This is the chessboard that will eventually be passed through to the engine class for evaluation
+    public static String[][] oldChessboard = new String[8][8];
+    public static HashMap<Integer, String> pieceCorrelations = new HashMap<Integer, String>(); //This stores the pixel values for each piece
+    public static double time = 0; // Creates a timer to check how long it takes to run the eval.
 
 
 
+    public static void main(String[] args) {
+        time = System.nanoTime(); // Starts the timer
+        addPiecePixelValues(); //Adds each pieces pixel counts to pieceCorrelations hashmap
+        checkForPieces();      //Checks the on-screen board for pieces
+        printChessBoard(chessboard);
+        setUpOutPutWindow();
 
 
-
-        JFrame frame = new JFrame();
-        frame.setBounds(10, 10, 500, 500);
-        frame.setDefaultCloseOperation(3);
-        JPanel panel = new JPanel() {
-            @Override
-            public void paint(Graphics g) {
-                g.setColor(Color.red);
-                g.fillRect(0, 0, 500, 500);
+        printChessBoard(oldChessboard);
+        while(true){
+            if(!Arrays.deepEquals(chessboard, oldChessboard)){
+                panel.repaint();
+                frame.repaint();
+                for(int i = 0; i < chessboard.length; i++){
+                    for(int j = 0; j < chessboard[i].length; j++){
+                        oldChessboard[i][j] = chessboard[i][j];
+                    }
+                }
+            }else{
+                checkForPieces();
             }
-        };
-        frame.add(panel);
-        frame.setVisible(true);
+        }
     }
     public static void checkForPieces() {
         try {
@@ -103,6 +96,36 @@ public class Main {
             }
         }catch(Exception e){
             System.out.println(e);
+        }
+    }
+    public static void addPiecePixelValues(){
+        pieceCorrelations.put(28, "BR");//Black Rook
+        pieceCorrelations.put(54, "Bk");//Black knight
+        pieceCorrelations.put(31, "BB");//Black Bishop
+        pieceCorrelations.put(44, "BQ");//Black Queen
+        pieceCorrelations.put(43, "BK");//Black King
+        pieceCorrelations.put(27, "BP");//Black Pawn
+        pieceCorrelations.put(21, "WR");//White Rook
+        pieceCorrelations.put(42, "Wk");//White knight
+        pieceCorrelations.put(24, "WB");//White Bishop
+        pieceCorrelations.put(10, "WQ");//White Queen
+        pieceCorrelations.put(50, "WK");//White King
+        pieceCorrelations.put(19, "WP");//White Pawn
+        pieceCorrelations.put(0, "  "); //This is for empty squares, so they don't say 'null' when the array is printed. Cuz that's annoying
+    }
+    public static void setUpOutPutWindow(){
+        JFrame frame = new JFrame();
+        frame.setBounds(1000, 10, 500, 500);
+        frame.setDefaultCloseOperation(3);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+    public static void printChessBoard(String[][] board){
+        for (String[] strings : board) {
+            for (String string : strings) {
+                System.out.print(string + " ");
+            }
+            System.out.println();
         }
     }
 }
